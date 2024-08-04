8b9ed187c16f8c60426df098bf71f3baf89c93ce
NITHIGE <anthress@outlook.com>
2024-08-04 02:43:11 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -62,7 +62,7 @@ app.get('/', (req, res) => {
     res.sendFile(imagePath);
 });
 app.listen(port, () => {
-    console.log(`🔗 Listening to GlaceYT : http://localhost:${port}`);
+    console.log(`🔗 Diseñado por Anthres`);
 });
 
 
